f2c4e058021bcba8bda4a8f0794f81e784334728
rwhite83 <rwhite83@hotmail.com>
2019-11-10 13:59:05 -0800

    got folder seen in filezilla

---
+++ WindowsFormsApp6/ClientConnection.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+////using log4net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+//using Microsoft.RuleEngine;
+
+
+namespace WindowsFormsApp6
+{
+    public class ClientConnection
+    {
+        private TcpClient _controlClient;
+
+        private NetworkStream _controlStream;
+        private StreamReader _controlReader;
+        private StreamWriter _controlWriter;
+
+        private string _username;
+        private string _transferType;
+        private TcpListener _passiveListener;
+        private bool conn_type_passive;
+        private IPEndPoint _dataEndpoint;
+        private string _currentDirectory;
+        private string _root;
+        private TcpClient _dataClient;
+        private StreamReader _dataReader;
+        private StreamWriter _dataWriter;
+
+        public ClientConnection(TcpClient client)
+        {
+            _controlClient = client;
+
+            _controlStream = _controlClient.GetStream();
+
+            _controlReader = new StreamReader(_controlStream);
+            _controlWriter = new StreamWriter(_controlStream);
+        }
+
+        public void HandleClient(object obj)
+        {
+            _controlWriter.WriteLine("220 Service Ready.");
+            _controlWriter.Flush();
+            
+
+            string line;
+
+            try
+            {
+                while (!string.IsNullOrEmpty(line = _controlReader.ReadLine()))
+                {
+                    string response = null;
+
+                    string[] command = line.Split(' ');
+
+                    string cmd = command[0].ToUpperInvariant();
+                    string arguments = command.Length > 1 ? line.Substring(command[0].Length + 1) : null;
+
+                    if (string.IsNullOrWhiteSpace(arguments))
+                        arguments = null;
+
+                    if (response == null)
+                    {
+                        Console.WriteLine("" + cmd);
+                        switch (cmd)
+                        {
+                            case "USER":
+                                response = User(arguments);
+                                break;
+                            case "PASS":
+                                response = Password(arguments);
+                                break;
+                            case "CWD":
+                                response = ChangeWorkingDirectory(arguments);
+                                break;
+                            case "CDUP":
+                                response = ChangeWorkingDirectory("..");
+                                break;
+                            case "PWD":
+                                response = "257 \"/\" is current directory.";
+                                break;
+                            case "QUIT":
+                                response = "221 Service closing control connection";
+                                break;
+                            case "TYPE":
+                                string[] splitArgs = arguments.Split(' ');
+                                response = Type(splitArgs[0], splitArgs.Length > 1 ? splitArgs[1] : null);
+                                break;
+                            case "PORT":
+                                Console.WriteLine("port called");
+                                response = Port(arguments);
+                                break;
+                            case "PASV":
+                                response = Passive();
+                                break;
+                            case "LIST":
+                                response = List(arguments);
+                                break;
+
+                            default:
+                                response = "502 Command not implemented";
+                                break;
+                        }
+                    }
+
+                    if (_controlClient == null || !_controlClient.Connected)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        _controlWriter.WriteLine(response);
+                        _controlWriter.Flush();
+
+                        if (response.StartsWith("221"))
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        #region FTP Commands
+
+        private string User(string username)
+        {
+            _username = username;
+
+            return "331 Username ok, need password";
+        }
+
+        private string Password(string password)
+        {
+            if (true)
+            {
+                return "230 User logged in";
+            }
+            else
+            {
+                return "530 Not logged in";
+            }
+        }
+
+        private string Port(string hostPort)
+        {
+
+            Console.WriteLine("alkjfla;dkfjl;dkfjsdklfjads;lfkdslkfjdsfl;");
+
+            // _dataConnectionType = DataConnectionType.Active;
+
+            string[] ipAndPort = hostPort.Split(',');
+
+            byte[] ipAddress = new byte[4];
+            byte[] port = new byte[2];
+
+            for (int i = 0; i < 4; i++)
+            {
+                ipAddress[i] = Convert.ToByte(ipAndPort[i]);
+            }
+
+            for (int i = 4; i < 6; i++)
+            {
+                port[i - 4] = Convert.ToByte(ipAndPort[i]);
+            }
+
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(port);
+
+            IPAddress wubbs = new IPAddress(ipAddress);
+
+            for (int i = 0; i < port.Length; i++)
+            {
+                Console.WriteLine(port[i]);
+            }
+
+            ushort wtf = (ushort)BitConverter.ToInt16(port, 0);
+            
+
+            _dataEndpoint = new IPEndPoint(new IPAddress(ipAddress), wtf);
+
+            return "200 Data Connection Established";
+
+        }
+
+        private string Passive()
+        {
+
+            conn_type_passive = true;
+
+            IPAddress localAddress = ((IPEndPoint)_controlClient.Client.LocalEndPoint).Address;
+
+            _passiveListener = new TcpListener(localAddress, 0);
+            _passiveListener.Start();
+
+            IPEndPoint localEndpoint = (IPEndPoint)_passiveListener.LocalEndpoint;
+
+            byte[] address = localEndpoint.Address.GetAddressBytes();
+            short port = (short)localEndpoint.Port;
+
+            byte[] portArray = BitConverter.GetBytes(port);
+
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(portArray);
+
+            return string.Format("227 Entering Passive Mode ({0},{1},{2},{3},{4},{5})",
+                          address[0], address[1], address[2], address[3], portArray[0], portArray[1]);
+        }
+
+        private string List(string pathname)
+        {
+
+            if (pathname == null)
+            {
+                pathname = string.Empty;
+            }
+
+            pathname = new DirectoryInfo(Path.Combine("fiona", pathname)).FullName;
+
+            //if (IsPathValid(pathname))
+            //{
+            if (!conn_type_passive)
+            {
+                Console.WriteLine("test2");
+
+                _dataClient = new TcpClient();
+                Console.WriteLine(_dataEndpoint.Address);
+                _dataClient.BeginConnect(_dataEndpoint.Address, _dataEndpoint.Port, DoList, pathname);
+            }
+            else
+            {
+                _passiveListener.BeginAcceptTcpClient(DoList, pathname);
+            }
+
+            return string.Format("150 Opening {0} mode data transfer for LIST", conn_type_passive ? ": passive" : ": active");
+            //}
+
+            //            return "450 Requested file action not taken";
+        }
+
+        private void DoList(IAsyncResult result)
+        {
+            if (!conn_type_passive)
+            {
+                _dataClient.EndConnect(result);
+            }
+            else
+            {
+                _dataClient = _passiveListener.EndAcceptTcpClient(result);
+            }
+
+            string pathname = (string)result.AsyncState;
+
+            using (NetworkStream dataStream = _dataClient.GetStream())
+            {
+                _dataReader = new StreamReader(dataStream, Encoding.ASCII);
+                _dataWriter = new StreamWriter(dataStream, Encoding.ASCII);
+
+                IEnumerable<string> directories = Directory.EnumerateDirectories(pathname);
+
+                foreach (string dir in directories)
+                {
+                    DirectoryInfo d = new DirectoryInfo(dir);
+
+                    string date = d.LastWriteTime < DateTime.Now - TimeSpan.FromDays(180) ?
+                        d.LastWriteTime.ToString("MMM dd  yyyy") :
+                        d.LastWriteTime.ToString("MMM dd HH:mm");
+
+                    string line = string.Format("drwxr-xr-x    2 2003     2003     {0,8} {1} {2}", "4096", date, d.Name);
+
+                    _dataWriter.WriteLine(line);
+                    _dataWriter.Flush();
+                }
+
+                IEnumerable<string> files = Directory.EnumerateFiles(pathname);
+
+                foreach (string file in files)
+                {
+                    FileInfo f = new FileInfo(file);
+
+                    string date = f.LastWriteTime < DateTime.Now - TimeSpan.FromDays(180) ?
+                        f.LastWriteTime.ToString("MMM dd  yyyy") :
+                        f.LastWriteTime.ToString("MMM dd HH:mm");
+
+                    string line = string.Format("-rw-r--r--    2 2003     2003     {0,8} {1} {2}", f.Length, date, f.Name);
+
+                    _dataWriter.WriteLine(line);
+                    _dataWriter.Flush();
+                }
+
+                _dataClient.Close();
+                _dataClient = null;
+
+                _controlWriter.WriteLine("226 Transfer complete");
+                _controlWriter.Flush();
+            }
+        }
+
+        private bool IsPathValid(string path)
+        {
+            return path.StartsWith(_root);
+        }
+
+        private string Type(string typeCode, string formatControl)
+        {
+            string response = "500 ERROR";
+
+            switch (typeCode)
+            {
+                case "A":
+                case "I":
+                    _transferType = typeCode;
+                    response = "200 OK";
+                    break;
+                case "E":
+                case "L":
+                default:
+                    response = "504 Command not implemented for that parameter.";
+                    break;
+            }
+
+            if (formatControl != null)
+            {
+                switch (formatControl)
+                {
+                    case "N":
+                        response = "200 OK";
+                        break;
+                    case "T":
+                    case "C":
+                    default:
+                        response = "504 Command not implemented for that parameter.";
+                        break;
+                }
+            }
+
+            return response;
+        }
+
+        private string ChangeWorkingDirectory(string pathname)
+        {
+            return "250 Changed to new directory";
+        }
+
+        #endregion
+    }
+}
---
+++ WindowsFormsApp6/FtpServer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApp6
+{
+    public class FtpServer
+    {
+        private TcpListener _listener;
+
+        public FtpServer()
+        {
+        }
+
+        public void Start()
+        {
+            _listener = new TcpListener(IPAddress.Any, 21);
+            _listener.Start();
+            _listener.BeginAcceptTcpClient(HandleAcceptTcpClient, _listener);
+        }
+
+        public void Stop()
+        {
+            if (_listener != null)
+            {
+                _listener.Stop();
+            }
+        }
+
+        private void HandleAcceptTcpClient(IAsyncResult result)
+        {
+            _listener.BeginAcceptTcpClient(HandleAcceptTcpClient, _listener);
+            TcpClient client = _listener.EndAcceptTcpClient(result);
+
+            ClientConnection connection = new ClientConnection(client);
+
+            ThreadPool.QueueUserWorkItem(connection.HandleClient, client);
+        }
+    }
+}
